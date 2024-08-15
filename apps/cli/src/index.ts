3e67e2821b2ad67576cd83baa6e1fcadc15e2835
Alastair Zotos <alastairzotos@gmail.com>
2024-08-16 01:06:43 +0300

    Nicer cli

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,6 +1,10 @@
 import { createInterface } from 'readline';
 import EventSource from 'eventsource';
+import chalk from 'chalk';
 import { Response } from '@bitmetro/callisto';
+
+const cliSpinners = require('fix-esm').require('cli-spinners');
+const ora = require('fix-esm').require('ora')
 
 async function* questions(query: string) {
   const rl = createInterface({
@@ -19,13 +23,31 @@ async function* questions(query: string) {
 }
 
 async function run() {
+  process.stdout.write('\x1Bc\n'); 
+
   let threadId: string | undefined;
 
+  const callistoPrompt = chalk.blueBright(`[${chalk.blue('Callisto')}] `);
+  
   const handleQuery = (query: string) => new Promise<void>(resolve => {
     const url = `http://localhost:6000/api/v1/chat?q=${encodeURIComponent(query)}${threadId ? `&tid=${threadId}` : ''}`;
     const ev = new EventSource(url);
+    
+    const spinner = ora({
+      prefixText: callistoPrompt,
+      spinner: cliSpinners.aesthetic,
+    }).start();
+
+    let responseStarted = false;
 
     ev.onmessage = (evt) => {
+      spinner.stop();
+
+      if (!responseStarted) {
+        process.stdout.write(callistoPrompt);
+        responseStarted = true;
+      }
+
       const res = JSON.parse(evt.data) as Response;
 
       switch (res.type) {
@@ -34,7 +56,7 @@ async function run() {
           break;
 
         case 'text':
-          process.stdout.write(res.data);
+          process.stdout.write(chalk.cyan(res.data));
           break;
 
         case 'data':
@@ -49,7 +71,7 @@ async function run() {
     };
   })
 
-  for await (const query of questions("> ")) {
+  for await (const query of questions(chalk.greenBright(`[${chalk.green('You')}] `))) {
     await handleQuery(query);
   }
 }
